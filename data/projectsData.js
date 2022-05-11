047bb1da93e8e586182a58dfe027e034e02ac016
Adrian Moses <adrian@adrianmoses.com>
2022-05-10 21:49:10 -0400

    starting out with goals page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Virtus',
-    description: `digital tools for managing mental health`, 
+    description: `digital tools for managing mental health`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
