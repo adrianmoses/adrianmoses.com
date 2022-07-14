89d5b8fc9ef3144e366ea8d4e224d9991b499040
Adrian Moses <adrian@adrianmoses.com>
2022-07-14 10:33:23 -0700

    add tinyletter

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -4,40 +4,20 @@ import siteMetadata from '@/data/siteMetadata'
 
 const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
   const inputEl = useRef(null)
-  const [error, setError] = useState(false)
-  const [message, setMessage] = useState('')
-  const [subscribed, setSubscribed] = useState(false)
-
-  const subscribe = async (e) => {
-    e.preventDefault()
-
-    const res = await fetch(`/api/${siteMetadata.newsletter.provider}`, {
-      body: JSON.stringify({
-        email: inputEl.current.value,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-
-    const { error } = await res.json()
-    if (error) {
-      setError(true)
-      setMessage('Your e-mail adress is invalid or you are already subscribed!')
-      return
-    }
-
-    inputEl.current.value = ''
-    setError(false)
-    setSubscribed(true)
-    setMessage('Successfully! 🎉 You are now subscribed.')
-  }
+  // const [error, setError] = useState(false)
+  // const [message, setMessage] = useState('')
+  // const [subscribed, setSubscribed] = useState(false)
 
   return (
     <div>
       <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</div>
-      <form className="flex flex-col sm:flex-row" onSubmit={subscribe}>
+      <form
+        className="flex flex-col sm:flex-row"
+        method="post"
+        action="https://tinyletter.com/adrianmoses"
+        target="popupwindow"
+        onSubmit="window.open('https://tinyletter.com/adrianmoses', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"
+      >
         <div>
           <label className="sr-only" htmlFor="email-input">
             Email address
@@ -47,28 +27,24 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
             className="px-4 rounded-md w-72 dark:bg-black focus:outline-none focus:ring-2 focus:border-transparent focus:ring-primary-600"
             id="email-input"
             name="email"
-            placeholder={subscribed ? "You're subscribed !  🎉" : 'Enter your email'}
+            placeholder={'Enter your email'}
             ref={inputEl}
             required
             type="email"
-            disabled={subscribed}
           />
         </div>
         <div className="flex w-full mt-2 rounded-md shadow-sm sm:mt-0 sm:ml-3">
           <button
-            className={`py-2 sm:py-0 w-full bg-primary-500 px-4 rounded-md font-medium text-white ${
-              subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
-            } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 dark:ring-offset-black`}
+            className={`py-2 sm:py-0 w-full bg-primary-500 px-4 rounded-md font-medium text-white hover:bg-primary-700 dark:hover:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 dark:ring-offset-black`}
             type="submit"
-            disabled={subscribed}
           >
-            {subscribed ? 'Thank you!' : 'Sign up'}
+            {'Sign up'}
           </button>
         </div>
       </form>
-      {error && (
-        <div className="pt-2 text-sm text-red-500 w-72 sm:w-96 dark:text-red-400">{message}</div>
-      )}
+      {/*{error && (*/}
+      {/*  <div className="pt-2 text-sm text-red-500 w-72 sm:w-96 dark:text-red-400">{message}</div>*/}
+      {/*)}*/}
     </div>
   )
 }
